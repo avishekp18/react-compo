b8207374730af16fbef54fa97e25e7220bb10cb9
avishekp18 <158828088+avishekp18@users.noreply.github.com>
2025-02-18 00:51:59 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  return (
+    <>
+    <h1>Hello Avishek!.</h1>
+    <p>From Today i'm going to change my life whole proces, to build my self for reputate person on on my eye.<hr></hr>
+    1% better every day = 365% better in a year!
+    </p>
+    </>
+);
+}
+
+export default App;
